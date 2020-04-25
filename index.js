e0d386bfd83626a628772ac8762333dfa2f32a2b
Daniel Westlund <dwestlund531@gmail.com>
2020-04-25 15:52:29 -0500

    math is working. numbers turn red if incorrect. Check answer will work even with multiple check answer buttons

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+let answerSheet = [];
 
-
-
-
-function getAnswer(number1,number2){
+function getAnswer(number1, number2) {
   return number1 + number2
 }
 
-function problemHTML(number1,number2,answer,i){
-  return`
+function problemHTML(number1, number2, answer, i) {
+  answerSheet.push({
+    [i]: answer
+  });
+  return `
 
   <div class='problem'>
   <p class='first'>${number1}</p>
@@ -15,66 +16,87 @@ function problemHTML(number1,number2,answer,i){
   <p class='third'>${number2}</p>
   <div class='fourth'><hr></div>
   <input class='answer ${i}' type='number'>
-  <button type='button' class='check_answer ${i}' val='${answer}'>Check Answer</button>
-  <input class='divAnswer hidden' val='${answer}'>
+  <button type='button' class='check_answer ${i}' >Check Answer</button>
+  <input class='divAnswer' type='hidden' value='${answer}'>
   </div>
   `
 }
 
 
-function generateHTML1(){
+function generateHTML1() {
   let stringHTML = '';
 
-  for(let i = 0; i< 2;i++){
+  for (let i = 0; i < 2; i++) {
     let number1 = Math.floor((Math.random() * 20) + 1);
     let number2 = Math.floor((Math.random() * 20) + 1);
-    let answer = getAnswer(number1,number2);
-    stringHTML += problemHTML(number1,number2,answer,i)
+    let answer = getAnswer(number1, number2);
+    if (number1 >= number2) {
+      stringHTML += problemHTML(number1, number2, answer, i)
+    } else {
+      stringHTML += problemHTML(number2, number1, answer, i)
+    }
+
   }
   $('#problems').html(stringHTML);
-
+  console.log(answerSheet);
 }
 
 
-function generateProblems(userRequest){
+function generateProblems(userRequest) {
   let HTML = '';
   console.log('generateProblems is working')
-  if(userRequest==='grade1'){
+  if (userRequest === 'grade1') {
     console.log('grade one is working')
     HTML += generateHTML1();
   }
-  if(userRequest==='grade2'){
+  if (userRequest === 'grade2') {
     HTML += generateHTML2();
   }
-  if(userRequest==='grade3'){
+  if (userRequest === 'grade3') {
     HTML += generateHTML3();
   }
-  generateHTML1(HTML);
+
 }
 
-function createMath(){
-  $('#search').on('click', function(){
+function createMath() {
+  $('#search').on('click', function () {
     event.preventDefault();
-  console.log('createMath Function is working')
-  const userRequest = 'grade1'
-  generateProblems(userRequest);
-})
-}
-
-function checkAnswer(){
-
-    $('#problems').on('click','.check_answer', function(){
-    event.preventDefault();
-    console.log(event.currentTarget);
-    let userAnswer = $('.answer').val();
-    let correctAnswer = $('.divAnswer').val();
-    console.log(correctAnswer);
-    console.log(userAnswer);
-
+    console.log('createMath Function is working')
+    const userRequest = 'grade1'
+    generateProblems(userRequest);
   })
 }
 
+function checkAnswer2() {
+  $('#problems').on('click', '.check_answer', function () {
+    event.preventDefault();
+    let userAnswer = $(this).parent().find('.answer').val();
+    let correctAnswer = $(this).parent().find('.divAnswer').val();
+    let userGrade = validateAnswer(userAnswer, correctAnswer);
+    if (userGrade === true) {
+      $(this).parent().css('color','black');
+    } 
+    else{
+      $(this).parent().css('color','red');
+    }
+  })
+};
 
 
-$(checkAnswer);
+function validateAnswer(userAnswer, correctAnswer) {
+  if (userAnswer == correctAnswer) {
+    console.log('right');
+    return true;
+  }
+  if (userAnswer != correctAnswer) {
+    console.log('wrong');
+    return false;
+  };
+};
+
+
+
+
+$(checkAnswer2);
+// $(checkAnswer);
 $(createMath);
