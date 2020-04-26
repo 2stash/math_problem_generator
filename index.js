f43b458b4c0b103cfa07064cc9dba0cd13d80003
Daniel Westlund <dwestlund531@gmail.com>
2020-04-25 21:51:18 -0500

    adding, subtracting, division, multiplication working. 1-3 digit working. User can input # of problems working. Check answer working

--- index.js
+++ index.js
@@ -1,83 +1,99 @@
-let answerSheet = [];
-
-function getAnswer(number1, number2) {
-  return number1 + number2
+function getAnswer(number1, number2, typeOfMath1) {
+  if (typeOfMath1 === '+') {
+    return number1 + number2
+  }
+  if (typeOfMath1 === '-') {
+    return number1 - number2
+  }
+  if (typeOfMath1 === '/') {
+    return number1 / number2
+  }
+  if (typeOfMath1 === '*') {
+    return number1 * number2
+  }
 }
 
-function problemHTML(number1, number2, answer, i) {
-  answerSheet.push({
-    [i]: answer
-  });
-  return `
+function problemHTML(numberArray,typeOfMath1) {
+  let htmlHTML = '';
 
+  htmlHTML += `
   <div class='problem'>
-  <p class='first'>${number1}</p>
-  <p class='second'>+</p>
-  <p class='third'>${number2}</p>
+  <p class='first'>${numberArray[0]}</p>
+  <p class='second'>${typeOfMath1}</p>
+  <p class='third'>${numberArray[1]}</p>
   <div class='fourth'><hr></div>
-  <input class='answer ${i}' type='number'>
-  <button type='button' class='check_answer ${i}' >Check Answer</button>
-  <input class='divAnswer' type='hidden' value='${answer}'>
+  <input class='answer' type='number'>
+  <button type='button' class='check_answer' >Check Answer</button>
+  <input class='divAnswer' type='hidden' value='${numberArray[2]}'>
   </div>
   `
+  return htmlHTML
+}
+
+function generateNumbers(typeOfMath1, typeOfMath2) {
+  let x = 0;
+  if (typeOfMath2 === 'oneDigit') {
+    x += 1;
+  } else if (typeOfMath2 === 'twoDigit') {
+    x += 11;
+  } else if (typeOfMath2 === 'threeDigit') {
+    x += 111;
+  }
+  let numberArray = [];
+  let number1 = Math.floor((Math.random() * 9 * x) + 1);
+  let number2 = Math.floor((Math.random() * 9 * x) + 1);
+  let answer = getAnswer(number1, number2, typeOfMath1);
+  console.log(answer);
+  if (number1 >= number2) {
+    numberArray.push(number1);
+    numberArray.push(number2);
+    numberArray.push(answer);
+
+  } else {
+    numberArray.push(number2);
+    numberArray.push(number1);
+    numberArray.push(answer);
+  }
+  return numberArray
 }
 
 
-function generateHTML1() {
-  let stringHTML = '';
+function generateProblems(typeOfMath1, typeOfMath2,typeOfMath3) {
 
-  for (let i = 0; i < 2; i++) {
-    let number1 = Math.floor((Math.random() * 20) + 1);
-    let number2 = Math.floor((Math.random() * 20) + 1);
-    let answer = getAnswer(number1, number2);
-    if (number1 >= number2) {
-      stringHTML += problemHTML(number1, number2, answer, i)
-    } else {
-      stringHTML += problemHTML(number2, number1, answer, i)
-    }
-
+  let numberArray = [];
+  let html = '';
+  for (let i = 0; i < typeOfMath3; i++) {
+    numberArray = generateNumbers(typeOfMath1, typeOfMath2);
+    html += problemHTML(numberArray, typeOfMath1);
   }
-  $('#problems').html(stringHTML);
-  console.log(answerSheet);
-}
-
-
-function generateProblems(userRequest) {
-  let HTML = '';
-  console.log('generateProblems is working')
-  if (userRequest === 'grade1') {
-    console.log('grade one is working')
-    HTML += generateHTML1();
-  }
-  if (userRequest === 'grade2') {
-    HTML += generateHTML2();
-  }
-  if (userRequest === 'grade3') {
-    HTML += generateHTML3();
-  }
-
+  $('#problems').html(html);
 }
 
 function createMath() {
   $('#search').on('click', function () {
     event.preventDefault();
     console.log('createMath Function is working')
-    const userRequest = 'grade1'
-    generateProblems(userRequest);
+
+    const typeOfMath1 = ($('input[name=type]:checked', '#myForm').val());
+    const typeOfMath2 = ($('input[name=digits]:checked', '#myForm').val());
+    const typeOfMath3 = ($('#howManyProblems').val());
+    console.log(typeOfMath1);
+    console.log(typeOfMath2);
+    console.log(typeOfMath3);
+    generateProblems(typeOfMath1, typeOfMath2,typeOfMath3);
   })
 }
 
-function checkAnswer2() {
+function checkAnswer() {
   $('#problems').on('click', '.check_answer', function () {
     event.preventDefault();
     let userAnswer = $(this).parent().find('.answer').val();
     let correctAnswer = $(this).parent().find('.divAnswer').val();
     let userGrade = validateAnswer(userAnswer, correctAnswer);
     if (userGrade === true) {
-      $(this).parent().css('color','black');
-    } 
-    else{
-      $(this).parent().css('color','red');
+      $(this).parent().css('color', 'green');
+    } else {
+      $(this).parent().css('color', 'red');
     }
   })
 };
@@ -97,6 +113,5 @@ function validateAnswer(userAnswer, correctAnswer) {
 
 
 
-$(checkAnswer2);
-// $(checkAnswer);
+$(checkAnswer);
 $(createMath);
